ca64fbb3758202eb92ab3bd8ef3156360f3530a0
junghun bak <wjdgnsi79@naver.com>
2022-12-23 00:12:25 +0900

    1223

--- a_concentrated_helper/Model/muiscPlayModel.swift
+++ a_concentrated_helper/Model/muiscPlayModel.swift
@@ -6,37 +6,24 @@
 //
 
 import Foundation
-import AVFoundation
 
 
 struct muiscPlayModel {
     
-    var player: AVAudioPlayer?
+    var listNumber = 1
     
-    mutating func playSound(list:Int) {
-            guard let url = Bundle.main.url(forResource: String(list), withExtension: "wav") else { return }
-
-            do {
-                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-                try AVAudioSession.sharedInstance().setActive(true)
-
-                /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
-                player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-                
-                print(url)
-
-                /* iOS 10 and earlier require the following line:
-                player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
-
-                guard let player = player else { return }
-
-                player.play()
-
-            } catch let error {
-                print(error.localizedDescription)
-            }
+    mutating func playList(){
+        if listNumber <= 3 {
+            self.listNumber += 1
+        }else {
+            self.listNumber = 0
+        }
         
     }
+        
+       
+        
+    
     
 
 
